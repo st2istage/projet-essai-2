2a6badfb56c0e7c2a62168e18aa2e5bbc1b86f85
HamzaSaidi0 <Hamzasaidi936@gmail.com>
2024-06-12 11:58:27 +0200

    changements hamza

--- projetbackend/src/main/java/com/projet/back/model/Article.java
+++ projetbackend/src/main/java/com/projet/back/model/Article.java
@@ -1,5 +1,6 @@
 package com.projet.back.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,7 +18,9 @@ public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String title;
+    @Column
     private String content;
 
     
--- projetbackend/src/main/java/com/projet/back/repository/ArticleRepository.java
+++ projetbackend/src/main/java/com/projet/back/repository/ArticleRepository.java
@@ -2,7 +2,10 @@ package com.projet.back.repository;
 
 import com.projet.back.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
+
 
